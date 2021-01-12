86b718cc0f9463eb08baa9c8a76a9aa429bb3c6b
jysfc <yaoj415@gmail.com>
2021-01-12 01:10:57 -0800

    half way into 340a, whitebear 03 select user

--- server.js
+++ server.js
@@ -4,34 +4,29 @@ const connection = mysql.createConnection({
    host: process.env.RDS_HOST,
    user: process.env.RDS_USER,
    password: process.env.RDS_PASSWORD,
-   database: "hobby_app",
+   database: "white_bear_app",
 });
 
 connection.connect();
 
-connection.query(
-   `
-   SELECT 
-      users.id AS user_id, 
-      users.first_name, 
-      users.last_name, 
-      hobbies.id AS hobby_id, 
-      hobbies.name AS hobby_name    
-   FROM 
-      users 
-         INNER JOIN 
-      xref_user_hobbies ON user_id = users.id 
-         INNER JOIN 
-      hobbies ON hobbies.id = xref_user_hobbies.hobby_id 
-   WHERE 
-      users.first_name = 'mike'`,
-   (err, res) => {
-      if (err) {
-         console.log(err);
-      } else {
-         console.log(res);
-      }
+function queryUser(email, password) {
+   return `
+   SELECT
+      id, email, created_at
+   FROM
+      users
+   WHERE
+      email = '${email}'
+      AND password = '${password}'
+   LIMIT 1;
+`;
+}
+connection.query(queryUser("mike@gmail.com", "replace_me"), (err, res) => {
+   if (err) {
+      console.log(err);
+   } else {
+      console.log(res);
    }
-);
+});
 
 connection.end();
