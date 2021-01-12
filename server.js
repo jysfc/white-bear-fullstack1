c216bee3f8ce7e33a3bbea14816069aaafdcbbcb
jysfc <yaoj415@gmail.com>
2021-01-11 20:47:23 -0800

    added mysql table query

--- server.js
+++ server.js
@@ -4,17 +4,34 @@ const connection = mysql.createConnection({
    host: process.env.RDS_HOST,
    user: process.env.RDS_USER,
    password: process.env.RDS_PASSWORD,
-   database: "white_bear_app",
+   database: "hobby_app",
 });
 
 connection.connect();
 
-connection.query("SELECT 1 + 1 AS solution", (error, results, fields) => {
-   if (error) {
-      console.log(error);
-   } else {
-      console.log("The solution is: ", results[0].solution);
+connection.query(
+   `
+   SELECT 
+      users.id AS user_id, 
+      users.first_name, 
+      users.last_name, 
+      hobbies.id AS hobby_id, 
+      hobbies.name AS hobby_name    
+   FROM 
+      users 
+         INNER JOIN 
+      xref_user_hobbies ON user_id = users.id 
+         INNER JOIN 
+      hobbies ON hobbies.id = xref_user_hobbies.hobby_id 
+   WHERE 
+      users.first_name = 'mike'`,
+   (err, res) => {
+      if (err) {
+         console.log(err);
+      } else {
+         console.log(res);
+      }
    }
-});
+);
 
 connection.end();
