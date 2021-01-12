86450a2d0edd67a31b8ea6a43f65e80d72cc0fb9
jysfc <yaoj415@gmail.com>
2021-01-12 11:40:52 -0800

    added selectUser query

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const mysql = require("mysql");
+const selectUser = require("./queries/selectUser");
+
 const connection = mysql.createConnection({
    host: process.env.RDS_HOST,
    user: process.env.RDS_USER,
@@ -9,19 +11,7 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-function queryUser(email, password) {
-   return `
-   SELECT
-      id, email, created_at
-   FROM
-      users
-   WHERE
-      email = '${email}'
-      AND password = '${password}'
-   LIMIT 1;
-`;
-}
-connection.query(queryUser("mike@gmail.com", "replace_me"), (err, res) => {
+connection.query(selectUser("mike@gmail.com", "replace_me"), (err, res) => {
    if (err) {
       console.log(err);
    } else {
