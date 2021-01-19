74ca94957157995f1eee93c09e87e6633a5eb938
jysfc <yaoj415@gmail.com>
2021-01-19 13:50:30 -0800

    deployed to heroku

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use("/api/v1/users", require("./api/v1/users"));
 app.use("/api/v1/memory-cards", require("./api/v1/memory-cards"));
-app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 3045;
 app.listen(port, () =>
